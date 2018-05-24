f5550271ad667952c805749f7e149052b3c16272
Rafhack <rafael.flip@hotmail.com>
2018-05-24 02:29:18 -0300

    First

---
+++ app/src/main/java/com/firebaseapp/rift_scuttler_c3e45/riftscuttler/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.firebaseapp.rift_scuttler_c3e45.riftscuttler
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
