74bc259071b8ad8c4aa4159edd8c4da3e24485d0
Rafhack <rafael.flip@hotmail.com>
2018-05-24 05:01:37 -0300

    Search Screen - Regions

--- app/src/main/java/com/firebaseapp/rift_scuttler_c3e45/riftscuttler/MainActivity.kt
+++ app/src/main/java/com/firebaseapp/rift_scuttler_c3e45/riftscuttler/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.firebaseapp.rift_scuttler_c3e45.riftscuttler
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import kotterknife.bindView
 
 class MainActivity : AppCompatActivity() {
+
+    private val spnRegions by bindView<Spinner>(R.id.activity_main_spn_regions)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupSpinner()
+    }
+
+    private fun setupSpinner() {
+        val adapter = ArrayAdapter.createFromResource(this, R.array.regions, R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
+        spnRegions.adapter = adapter
     }
 }
